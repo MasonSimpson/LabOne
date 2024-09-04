a498559539d243e7cd002cc32784833dd476459c
MasonSimpson <masonrsimpson@gmail.com>
2024-09-04 00:05:25 -0500

    Still work in progress

--- src/PursePanel.java
+++ src/PursePanel.java
@@ -7,21 +7,24 @@ public class PursePanel extends JPanel {
     public Purse purse = new Purse();
     public JPanel resetDisplayPanel;
     //private JPanel pursePanel = new JPanel();
+    public boolean active;
 
     public PursePanel() {
         this.setPreferredSize(new Dimension(400, 300));
         this.setBackground(Color.cyan);
         resetDisplayPanel = new JPanel();
         resetDisplayPanel.setBackground(Color.cyan);
+        resetDisplayPanel.setPreferredSize(new Dimension(300,300));
+        resetDisplayPanel.setOpaque(true);
         this.add(resetDisplayPanel);
+
     }
 
     public void paintComponent(Graphics g) {
-
         super.paintComponent(g);
-        resetDisplayPanel = new JPanel();
-        this.remove(resetDisplayPanel);
-        resetDisplayPanel.setBackground(Color.cyan);
+        this.removeAll();
+        resetDisplayPanel.removeAll();
+        int x = 50, y = 50;
 
         for (Map.Entry<Denomination, Integer> entry : purse.cash.entrySet()) {
             for (int i = 1; i <= entry.getValue(); i++) {
@@ -29,9 +32,15 @@ public class PursePanel extends JPanel {
                 ImageIcon img = new ImageIcon(entry.getKey().img());
                 img.setImage(img.getImage().getScaledInstance(30, 30, Image.SCALE_SMOOTH)); // Image itself
                 purseLabel.setIcon(img);
-                purseLabel.setBounds(50, 50, 30, 30); // JPanel that contains image
+                purseLabel.setBounds(x, y, 30, 30); // JPanel that contains image
                 resetDisplayPanel.add(purseLabel);
                 this.add(resetDisplayPanel);
+                if (x >= 300) {
+                    x = 50;
+                    y += 30;
+                }
+                else
+                    x+=50;
             }
         }
     }
